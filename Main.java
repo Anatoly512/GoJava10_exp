245edf42932bc544689e1cbec48481277627db9a
Anatoly512 <56732684+Anatoly512@users.noreply.github.com>
2019-10-20 04:58:22 +0300

    Initial commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, world !  Wow !!! ");
+        System.out.println("\nСтрока 1\n\tСтрока 2\n\t\tCтрока 3");
+        System.out.println();
+            }
+}
+
+// Comment
+/*
+  Comment 2
+  Comment 3
+ */
